0dcfdff815b5f710cf6ffdc46909e498ebdf7a8c
Phu Vo <phu.vo@ffw.com>
2023-12-19 13:38:55 +0700

    Create nextjs and tailwindcss

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      maxWidth: {
+        "8xl": "1400px",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
